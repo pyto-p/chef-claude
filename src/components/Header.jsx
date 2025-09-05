18cf25605b82746d71b152dd7d6db24c885bf624
Sebastian Temporal <sebastian.audrey@verityintel.com>
2025-09-05 15:52:05 +0800

    feat: header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import chefClaudeIcon from "../assets/chef-claude-icon.png"
+
+function Header() {
+    return (
+        <header>
+            <img className="header-icon" src={chefClaudeIcon} />
+            <h1 className="header-title">Chef Claude</h1>
+        </header>
+            
+    )
+}
+
+export default Header
