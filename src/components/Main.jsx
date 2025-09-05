a1b49cdb9593a03db7881b020c407e4967439fd0
Sebastian Temporal <sebastian.audrey@verityintel.com>
2025-09-06 07:32:48 +0800

    feat: add ingredient state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-
+import { useState } from "react";
 function Main() {
+
+  const [ingredients, setIngredients] = useState([])
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+
+    setIngredients(prev => [...prev, newIngredient])
+  }
+
+  const ingredientsListItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+  ))
+
   return (
     <main>
-      <form>
-        <input className="input-bar" aria-label="Add ingredient" placeholder="e.g. oregano" />
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="e.g. oregano" 
+          className="input-bar" 
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
         <button className="input-btn">+ Add ingredient</button>
       </form>
+      <ul>
+        {ingredientsListItems}
+      </ul>
     </main>
   );
 }
