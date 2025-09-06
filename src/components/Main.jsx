132b1a4318e1e18eb1002990eadb349ebbc3715f
Sebastian Temporal <sebastian.audrey@verityintel.com>
2025-09-06 17:56:36 +0800

    feat: show ingredients list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,36 +1,53 @@
 import { useState } from "react";
 function Main() {
+  const [ingredients, setIngredients] = useState([]);
 
-  const [ingredients, setIngredients] = useState([])
+  const submit = (formData) => {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prev) => [...prev, newIngredient]);
+  };
 
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const newIngredient = formData.get("ingredient")
-
-    setIngredients(prev => [...prev, newIngredient])
-  }
-
-  const ingredientsListItems = ingredients.map(ingredient => (
+  const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
-  ))
+  ));
 
   return (
     <main>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          placeholder="e.g. oregano" 
-          className="input-bar" 
+      <form action={submit} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          className="input-bar"
           aria-label="Add ingredient"
           name="ingredient"
         />
         <button className="input-btn">+ Add ingredient</button>
       </form>
-      <ul>
-        {ingredientsListItems}
-      </ul>
+
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+          <div className="get-recipe-container">
+            {ingredients.length > 3 ? (
+              <>
+                <div>
+                  <h3>Ready for a recipe?</h3>
+                  <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button>Get a recipe</button>
+              </>
+            ) : (
+              <p>
+                You need {4 - ingredients.length} more ingredients to generate a
+                recipe!
+              </p>
+            )}
+          </div>
+        </section>
+      )}
     </main>
   );
 }
